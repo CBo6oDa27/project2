8be33447c6734947b90b7bb6e734a9fdf3e3d765
alexeyzakirov <zakirov.alexey@gmail.com>
2023-11-29 14:23:34 +0300

    second commit

--- funcs.py
+++ funcs.py
@@ -1,2 +1,7 @@
-def two_digits_summ():
-    two_digits = 10 + 110
+def two_digits_summ(number_1, number_2):
+    two_digits = number_1 + number_2
+    return two_digits
+
+def two_digits_diff(number_1, number_2):
+    two_digits = number_1 - number_2
+    return two_digits
---
+++ main.py
@@ -0,0 +1,3 @@
+from funcs import two_digits_summ
+
+two_digits_summ(10, 100)
